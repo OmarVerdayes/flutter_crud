8f3689f351e560bebc2ec0e8a56218f6e72f26c0
OmarVerdayes <105461930+OmarVerdayes@users.noreply.github.com>
2024-08-04 21:35:26 -0600

    Update createandcopytofolder.sh

--- createandcopytofolder.sh
+++ createandcopytofolder.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Elimina la carpeta artifacts si ya existe
 [[ -d artifacts ]] && rm -r artifacts
-mkdir artifacts
-cp -R build/web artifacts
+# Crea la carpeta artifacts y el subdirectorio web
+mkdir -p artifacts/web
+# Copia el contenido de build/web a artifacts/web
+cp -R build/web/* artifacts/web/
